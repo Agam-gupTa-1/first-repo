12941ea5a416c21fefc49230be0d5fe003a11f80
Agam Gupta <curiousagam@gmail.com>
2022-02-12 22:22:39 +0530

    Added main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include<iostream>
+#include"monster.h"
 #include<vector>
 using namespace std;
+
+
+void test (int,int);
+void feature(int);
 int main()
 {
-   
+   cout<<"NAME: AGAM GUPTA"<<endl;
    return 0;
 }
